39ecc9884b4b7bbaed54d590705fca5b3d0b0c6a
Shalom Harpaz <sh770h@gmail.com>
2023-09-14 17:20:41 +0300

    14/09

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from waitress import serve
 
 app = Flask(__name__)
 
@@ -14,4 +15,9 @@ def about():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
+    #app.run(host='0.0.0.0')
+    # serve(app, host='0.0.0.0', port=5000)
+    # serve(app, host='0.0.0.0', port=8081)
+    
